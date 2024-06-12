15043960684119fc4546fcefda52174825893a4c
bokney <toknyk@gmail.com>
2024-06-12 23:10:42 +0100

    working example

--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
 import tkinter as tk
 
-def main(*args):
-    window = None
-    window = tk.Tk()
-    assert window != None
-    window.mainloop()
+def button_pushed():
+    print('button has been pushed')
+    exit()
 
-main()
+window = None
+window = tk.Tk()
+assert window != None
+
+window.title = ''
+window.geometry('640x480')
+
+label = tk.Label(master=window,
+                 text="Welcome to this magnificent piece of software!!")
+label.pack()
+
+text = tk.Text(master=window)
+text.pack()
+
+label = tk.Label(master=window,
+                 text="What is going on?!")
+label.pack()
+
+entry = tk.Entry(master=window,
+                 width=48)
+entry.pack()
+
+button = tk.Button(master=window,
+                   text='⚠️Click Me If You Dare!!⚠️',
+                   command=button_pushed)
+button.pack()
+
+window.mainloop()
